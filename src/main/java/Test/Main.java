a21c90be3d51f6e375b64332812270b7bcb78e41
khalil <khalil.elmnari@esprit.tn>
2026-04-12 00:12:58 +0100

    verification architecture

--- src/main/java/Test/Main.java
+++ src/main/java/Test/Main.java
@@ -1,21 +1,11 @@
 package test;
 
 import entities.User;
-import services.ServicePersonne;
 
 import java.sql.SQLDataException;
 
 public class Main {
     public static void main(String[] args) {
-        ServicePersonne servicePersonne = new ServicePersonne();
-        try {
-            servicePersonne.add(new User("Yassine","Dhaya",90));
-            servicePersonne.add(new User("Rihem","MATTOUSI",190));
-            servicePersonne.add(new User("Falten","Foulen",70));
-            servicePersonne.update(new User("TEsting","Hmed",33));
-            System.out.println(servicePersonne.getAll());
-        } catch (SQLDataException e) {
-            throw new RuntimeException(e);
-        }
+
     }
 }
